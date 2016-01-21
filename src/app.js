50211ef966617f72005f0596a2fc3e83f6573ef8
Michael Guild <michaelguild13@gmail.com>
2016-01-21 14:10:00 -0500

    added obj def list for referencing where to scroll/section points

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
 
 export default class ParallaxNav {
   constructor(el) {
+    // Get Menu Parent
     this.el = el
+    // Get all Links
+    this.links = [].slice.call( el.children )
+    // object definition
+    this.sections = {}
 
-    for ( let key in el.children ) {
-      let child = el.children[key],
-          hash = el.children[key].hash || ''
-
-      hash ? child.addEventListener('click', this.clickHandler) : ''
-    }
+    this.links.map( link => {
+      if ( link.hash ) {
+        this.sections[link.hash] = document.querySelector(link.hash).offsetTop - el.scrollHeight
+        link.addEventListener('click', e => {
+          e.preventDefault()
+          this.clickHandler(e)
+        })
+      } else {
+        this.sections['#top'] = el.scrollHeight
+      }
+    })
   }
 
   clickHandler(e) {
-    // let hash = e.target.hash,
-    //     offsetTop = hash ? document.querySelector(hash).offsetTop : ''
-    console.log(e)
+    console.log(e.target.hash)
+    window.scroll( 0, this.sections[ e.target.hash ] )
 
-    // e.preventDefault()
   }
 }
 
-var parallaxNav = new ParallaxNav(document.querySelector('.vpsn'))
+window.parallaxNav = new ParallaxNav(document.querySelector('.vpsn'))
